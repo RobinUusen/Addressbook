28f8d358db562728a0da1e199ae3a2736c2bb8ff
user <robin.uusen@khk.ee>
2020-11-24 13:48:49 +0200

    Add contact to HTML table by person object and user interface object

---
+++ app.js
@@ -0,0 +1,33 @@
+// define variables
+// contact data
+const firstName = document.querySelector('#first_name');
+const lastName = document.querySelector('#last_name');
+const city = document.querySelector('#city');
+const street = document.querySelector('#street');
+const postCode = document.querySelector('#postcode');
+const phone = document.querySelector('#phone');
+const contact = [firstName, lastName, city, street, postCode, phone];
+
+// app data
+const form = document.querySelector('#contact-form');
+const contacts = document.querySelector('#contacts-table');
+
+// define event listeners
+// add contact to table - submit button
+form.addEventListener('submit', addContact);
+
+// project functions
+// addContact
+function addContact(e) {
+    if (firstName.value === '') {
+        alert("Add new contact data!")
+    } else {
+        console.log("Create contact");
+        const person = new Person(firstName.value, lastName.value, city.value, street.value, postCode.value, phone.value);
+       console.log("create user interface");
+       const ui = new UI();
+       ui. addPersonToTable(person);
+        console.log(contacts);
+        e.preventDefault();
+    }
+}
